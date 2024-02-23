d2ef8e4f65d34167e739ebe11c645fc73f97f6a7
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-02-23 13:55:44 +0700

    fixed navbar

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menu = document.getElementById("menu");
 const list = document.getElementsByClassName("list")[0];
 
-menu.addEventListener("click", function() {
+menu.addEventListener("click", function(e) {
     list.classList.toggle("active");
+    e.preventDefault();
+})
+
+// ketika di klik di luar close
+
+document.addEventListener("click", (e) => {
+    if (!menu.contains(e.target) && !list.contains(e.target))
+        list.classList.remove("active");
 })
